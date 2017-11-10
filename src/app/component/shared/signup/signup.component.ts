1fb1f27e2bc0fc63276979fb77c98f1c1543371b
S3gillu <shubham.saurabh@SDIRECT-1>
2017-11-10 18:06:30 +0530

    Conditional dropdown implemented

--- src/app/component/shared/signup/signup.component.ts
+++ src/app/component/shared/signup/signup.component.ts
@@ -11,6 +11,7 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 export class SignupComponent implements OnInit {
   closeResult: string;
   genders = genders;
+  maritalStatus=maritalStatus;
 
   ngOnInit() {
     //throw new Error('Method Not Implemented.');
@@ -18,6 +19,13 @@ export class SignupComponent implements OnInit {
   refresh() { }
 
   signupForm: FormGroup
+
+  
+  selectedCountry = 0;                       /** Cascading dropsown code starts */
+  selectedState = 0;
+  states = [];
+  cities = [];
+                                              /** Cascading dropsown code ends */
 
   constructor(fb: FormBuilder,  private modalService: NgbModal) {
     this.signupForm = fb.group({
@@ -30,7 +38,11 @@ export class SignupComponent implements OnInit {
       gender: ['', [Validators.required]],
       username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]],
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-      Confirmpassword:['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+      Confirmpassword:['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      country :['',[Validators.required]],
+      state:['',[Validators.required]],
+      city:['',[Validators.required]],
+      maritalStatus:['',[Validators.required]]
     }, { validator: this.checkIfMatchingPasswords('password', 'Confirmpassword') });
   }
   submitForm1(value: any) {
@@ -99,7 +111,68 @@ export class SignupComponent implements OnInit {
     }
   }
 
+ /** Cascading Dropdown Code Starts **/
+
+  onSelectCountry(country_id: number) {
+    this.selectedCountry = country_id;
+    this.selectedState = 0;
+    this.cities = [];
+    this.states = this.getStates().filter((item) => {
+      return item.country_id === Number(country_id)
+    });
+  }
+
+  onSelectState(state_id: number) {
+    this.selectedState = state_id;
+    this.cities = this.getCity().filter((item) => {
+      return item.state_id === Number(state_id)
+    });
+  }
+  getCountries() {
+    return [
+      { id: 1, name: 'India' },
+      { id: 2, name: 'USA' },
+      { id: 3, name: 'Australia' }
+    ];
+  }
+  getStates() {
+    return [
+      { id: 1, country_id: 1, name: 'Andhra Pradesh' },
+      { id: 2, country_id: 1, name: 'Madhya Pradesh' },
+      { id: 3, country_id: 2, name: 'San Francisco' },
+      { id: 4, country_id: 2, name: 'Los Angeles' },
+      { id: 5, country_id: 3, name: 'New South Wales' },
+      { id: 6, country_id: 3, name: 'Victoria' },
+    ]
+  }
+  getCity() {
+    return [
+      { id: 1, state_id: 1, name: 'Guntur' },
+      { id: 2, state_id: 1, name: 'Vijayawada' },
+      { id: 3, state_id: 1, name: 'Nellore' },
+      { id: 4, state_id: 1, name: 'Kadapa' },
+      { id: 5, state_id: 2, name: 'Warangal' },
+      { id: 6, state_id: 2, name: 'Hyderabad' },
+      { id: 7, state_id: 2, name: 'Karimnagar' },
+      { id: 8, state_id: 2, name: 'Kadapa' },
+      { id: 9, state_id: 3, name: 'SOMA' },
+      { id: 10, state_id: 3, name: 'Richmond' },
+      { id: 11, state_id: 3, name: 'Sunset' },
+      { id: 12, state_id: 4, name: 'Burbank' },
+      { id: 13, state_id: 4, name: 'Hollywood' },
+      { id: 14, state_id: 5, name: 'Sunset' },
+      { id: 15, state_id: 5, name: 'Burbank' },
+      { id: 16, state_id: 5, name: 'Hollywood' },
+      { id: 17, state_id: 6, name: 'Benalla' },
+      { id: 18, state_id: 6, name: 'Melbourne' },
+    ]
+  }
+
+  /** Cascading Dropdown Code ends **/
+
 }
 
 export const genders: any = ['Male', 'Female', 'Others'];
+
+export const maritalStatus: any = ['Married', 'Unmarried'];
 
